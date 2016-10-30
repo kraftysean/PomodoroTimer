ddc84bac139174cbfa3a3c643694605c045bea45
Sean M <smarnane@gmail.com>
2016-10-30 01:07:50 +0000

    Initialize a single instance of Timer, which schedules  a new TimerTask.

--- src/io/krafty/pomodoro/Pomodoro.java
+++ src/io/krafty/pomodoro/Pomodoro.java
@@ -1,23 +1,28 @@
 package io.krafty.pomodoro;
 
+import java.time.LocalTime;
 import java.util.Timer;
 import java.util.TimerTask;
 
 /**
- * Created by sean on 29/10/16.
+ * Created by Sean on 29/10/16.
  */
 public class Pomodoro {
 
     public static void main(String[] args) {
-        Timer timer = new Timer();
-        TimerTask task = new TimerTask() {
+        final Timer timer = new Timer();
+        LocalTime tCounter = LocalTime.of(0, 0, 5);
+        timer.scheduleAtFixedRate(new TimerTask() {
+            int secondsRemaining = tCounter.toSecondOfDay();
             @Override
             public void run() {
+                System.out.printf("%02d:%02d\n", secondsRemaining / 60, secondsRemaining % 60);
+                if(--secondsRemaining < 0) {
+                    timer.cancel();
+                    System.out.println("Timer Ended");
+                }
 
             }
-        };
-        long delay = 0L;
-        long period = 0L;
-        timer.scheduleAtFixedRate(task, delay, period);
+        }, 0, 1000);
     }
 }
